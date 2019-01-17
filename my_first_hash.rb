4c23c754715add895da1b7666a8a0255b52d58f6
Derek Cerretani <derekcerretani@gmail.com>
2019-01-17 18:16:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -56,3 +56,13 @@ def shipping_manifest
     "oil paintings" => 3
   }
 end
+
+def shipping_manifest
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
+
+  the_manifest["oil paintings"]
+end
