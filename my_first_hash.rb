9d50ede3cc7693d8227db400305c7e49b961a513
Derek Cerretani <derekcerretani@gmail.com>
2019-01-17 18:25:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -74,6 +74,9 @@ def adding
     "oil paintings" => 3
   }
 
-  shipping_manifest["muskets"] = 2
-  shipping_manifest["gun powder"] = 4
+  the_manifest["muskets"] = 2
+  the_manifest["gun powder"] = 4
+
+  the_manifest
+  
 end
